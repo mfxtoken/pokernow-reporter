09aa347251d9a70f9e1f82a8f8fd237174544e81
[mfxtoken] <mfinityfx@gmail.com>
2025-11-27 01:04:00 +0530

    Pratik Shah Changed

--- src/App.jsx
+++ src/App.jsx
@@ -183,7 +183,7 @@ export default function PokerNowReporter() {
     'sas': 'Simal Shah',
     'mani27': 'Manigandan Manjunathan',
     'gp': 'Gaurav Jain',
-    'pratik': 'Pratik Jain',
+    'pratik': 'Pratik Shah',
     'kd': 'Keval Desai',
     'harshit': 'Harshit Metha',
     'shivang': 'Shivang',
